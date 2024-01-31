7e220faedfe5a6e5ea45b6c45386e0e32c068f9a
kennycummings <kennethccummings@gmail.com>
2024-01-31 18:25:50 -0500

    Updated header and debugged site so more pages would appear.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -7,8 +8,20 @@ const Header = () => {
       <div className="portfolio-container float-left">
         <img src="assets/images/Kenny-Cummings-LinkedIn-Photo.png" alt="Kenny Cummings LinkedIn Profile Picture" style={{ width: '75px', height: '75px' }} />
       </div>
-
-      {/* ... (insert rest of header JSX) */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          {/* Add more links as needed */}
+        </ul>
+      </nav>
     </div>
   );
 };
