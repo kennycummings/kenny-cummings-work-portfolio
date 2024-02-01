0228475a2c2550be03bdd9a846ca55dc9c0fad8f
kennycummings <kennethccummings@gmail.com>
2024-01-31 20:30:35 -0500

    Updated styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         </div>
 
         <div className="right">
-          <h5>
+          <h4>
             <a
               href="https://www.linkedin.com/in/kennycummings/"
               target="_blank"
@@ -23,7 +23,7 @@ const Contact = () => {
             >
               LinkedIn
             </a>
-          </h5>
+          </h4>
           <h5 style={{ color: 'whitesmoke' }}>kennethccummings@gmail.com</h5>
           <h5 style={{ color: 'whitesmoke' }}>215.432.0306</h5>
         </div>
