fc654263aad6e7f7e3253c069dc6a863aa8bff74
kennycummings <kennethccummings@gmail.com>
2024-02-01 14:46:16 -0500

    Fixed syntax in Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,16 +3,13 @@ import React from 'react';
 const Projects = () => {
   return (
     <div>
-
       <div className="web-development-projects">
         <h2 id="web-development-projects">Web Development Projects</h2>
 
         <br />
         <br />
 
-        <h3>
-          Round on Me
-        </h3>
+        <h3>Round on Me</h3>
 
         <a href="https://round-on-me-d919eb5aa07c.herokuapp.com/" target="_blank" rel="noopener noreferrer">
           <img src="assets/images/round-on-me-homepage.png" alt="Round on Me" style={{ width: '100%' }} />
@@ -21,17 +18,15 @@ const Projects = () => {
         <br />
         <br />
 
-        <p>
-            <h4><b>Check out my golfer meet-up app:</b></h4>
-            <h4><b><a href="https://round-on-me-d919eb5aa07c.herokuapp.com/" target="_blank" rel="noopener noreferrer">Round on Me</a></b></h4>
-        </p>
+        <div>
+          <h4><b>Check out my golfer meet-up app:</b></h4>
+          <h4><b><a href="https://round-on-me-d919eb5aa07c.herokuapp.com/" target="_blank" rel="noopener noreferrer">Round on Me</a></b></h4>
+        </div>
 
         <br />
         <br />
 
-        <h3>
-          Your Nutrition Radar
-        </h3>
+        <h3>Your Nutrition Radar</h3>
 
         <a href="https://mykhailozakh.github.io/Your-Nutrition-Radar/#" target="_blank" rel="noopener noreferrer">
           <img src="assets/images/Your-Nutrition-Radar-Home.png" alt="Your Nutrition Radar" style={{ width: '100%' }} />
@@ -40,11 +35,10 @@ const Projects = () => {
         <br />
         <br />
 
-        <p>
-            <h4><b>Check out my diet and nutrition app:</b></h4>
-            <h4><b><a href="https://mykhailozakh.github.io/Your-Nutrition-Radar/#" target="_blank" rel="noopener noreferrer">Your Nutrition Radar</a></b></h4>
-        </p>
-
+        <div>
+          <h4><b>Check out my diet and nutrition app:</b></h4>
+          <h4><b><a href="https://mykhailozakh.github.io/Your-Nutrition-Radar/#" target="_blank" rel="noopener noreferrer">Your Nutrition Radar</a></b></h4>
+        </div>
       </div>
 
       <br />
