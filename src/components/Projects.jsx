e7b496aaa7d033c0fcac4d1c18f63e634df9efa3
kennycummings <kennethccummings@gmail.com>
2024-02-04 15:47:57 -0500

    Added work experience to Project.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -69,7 +69,33 @@ const Projects = () => {
       </div>
 
       <div className="container">
-        {/* Add content for web content and development work experience */}
+        <div className="image-container">
+          <img src="./assets/images/AWMA-homepage.png" alt="AWMA Homepage Navigation Edit" style={{ width: '100%' }} className="image" />
+        </div>
+        <div className="text-container">
+          <h4>AWMA - Web Content Specialist</h4>
+          <p>
+            <ul>
+              <li>Achieved an impressive 100% increase in clicks, impressions, and click-through-rate over a 6-month
+                period by implementing strategic enhancements such as new copy, metadata optimization, navigation
+                improvements, and best SEO practices</li>
+              <li>Led a critical project overseeing the seamless website host migration from Shopify to Magento and
+                designing a captivating new homepage for the Magento site</li>
+              <li>Nurtured business partnerships with NetElixir to strengthen SEO, site mapping, and content
+                strategies</li>
+              <li>Managed website content and apps, ensuring content accuracy and resolving technical issues</li>
+              <li>Proficiently utilized Google Analytics, Google My Business, and Google Ads to drive data-informed
+                decisions and executed successful PPC campaigns with NetElixir</li>
+            </ul>
+            <b>Source: <a href="https://awma.com" target="_blank">AWMA Homepage</a></b>
+          </p>
+
+          <h4 style={{ textAlign: 'center' }}>
+            Achieved an impressive 100% increase in clicks, impressions, and click-through-rate over a 6-month
+            period by implementing strategic enhancements such as new copy, metadata optimization, navigation
+            improvements, and best SEO practices
+          </h4>
+        </div>
       </div>
 
       <br />
@@ -80,8 +106,38 @@ const Projects = () => {
       </div>
 
       <div className="container">
-        {/* Add content for copywriting and blogging work experience */}
+        <div className="text-container">
+          <h4>room service 360 - Ecommerce Content Specialist</h4>
+          <p>
+            <ul>
+              <li>Managed luxury furniture brands including Porada, Bonaldo, Gamma Arredamenti, and Nicoline
+                Italia, contributing to a significant expansion of product offerings.</li>
+              <li>Introduced over 500 new products, skillfully creating and maintaining product detail pages in
+                Magento.</li>
+              <li>Excelled in content curation, meticulously aligning published content with brand identity and
+                best SEO practices.</li>
+              <li>Utilized Adobe Photoshop for image editing and PDF spec sheet creation, ensuring top-notch
+                product presentation.</li>
+              <li>Played a pivotal role in configuring product filters and sorting mechanisms to enhance user
+                experience.</li>
+              <li>Demonstrated strong attention to detail by consistently updating product data and pricing for
+                accuracy.</li>
+              <li>Provided valuable customer support, offering design expertise that directly led to over $100,000
+                in sales.</li>
+              <li>Authored SEO-optimized blog content using WordPress.</li>
+            </ul>
+            <b>Source: <a href="https://www.roomservice360.com/inspiration/the-changing-of-the-seasons-fall-is-here/"
+              target="_blank">roomservice 360 Fall Blog</a></b>
+          </p>
+        </div>
+        <div className="image-container">
+          <img src="./assets/images/roomservice360-fall-blog.png" alt="roomservice 360 Fall Blog" style={{ width: '100%' }} className="image" />
+        </div>
       </div>
+
+      <h4 style={{ textAlign: 'center' }}>
+        Authored SEO-optimized blog content using WordPress.
+      </h4>
     </div>
   );
 };
