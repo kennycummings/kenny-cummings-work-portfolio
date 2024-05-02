0fe64bbc329ef584c2295c2285210e08caa32f1b
kennycummings <kennethccummings@gmail.com>
2024-05-02 16:01:01 -0400

    Updated homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home-container">
       <h2>Welcome to my portfolio!</h2>
       <p>
-        Hi, I'm Kenny Cummings, a passionate and results-driven Full Stack Web Developer with a diverse background in digital marketing, ecommerce, and product management.
+        Hi, I'm Kenny Cummings, a passionate and results-driven Full-Stack Web Developer with a diverse background in digital marketing, ecommerce, and product management.
         My expertise lies in JavaScript, CSS, HTML, CMS, and PIM. I have a proven track record of managing the entire product life cycle, implementing effective content/promotions, and driving sales growth.
         Explore my portfolio and work experience to learn more about my skills and projects!
       </p>
