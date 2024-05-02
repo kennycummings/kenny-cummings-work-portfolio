03181f3e4930e4a47ddc5340d921d9dbc8b28046
kennycummings <kennethccummings@gmail.com>
2024-05-02 15:42:48 -0400

    Updated Aboue Me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,7 +11,7 @@ const AboutMe = () => {
       <div className="content-container">
         <div className="text-container">
           <p>
-            Results-driven Full Stack Web Developer with a diverse background encompassing 3 years of web development expertise, coupled with 6 years of digital marketing, ecommerce, product management, and category management experience. Advanced in JavaScript, CSS, HTML, CMS, and PIM. Proficient in managing the entire product life cycle, implementing promotions, and driving sales growth. Adept at utilizing CMS, PIM, and Microsoft Excel for effective content and customer management. Demonstrated success as an Amazon and eBay seller. Highly analytical and adaptable, leveraging data analysis to provide actionable insights for product management. Proven track record of enhancing site visibility and Google performance through the implementation of innovative content strategies.
+            Results-driven Full-Stack Web Developer with a diverse background encompassing 3 years of web development expertise, coupled with 6 years of digital marketing, ecommerce, product management, and category management experience. Advanced in JavaScript, CSS, HTML, CMS, and PIM. Proficient in managing the entire product life cycle, implementing promotions, and driving sales growth. Adept at utilizing CMS, PIM, and Microsoft Excel for effective content and customer management. Demonstrated success as an Amazon and eBay seller. Highly analytical and adaptable, leveraging data analysis to provide actionable insights for product management. Proven track record of enhancing site visibility and Google performance through the implementation of innovative content strategies.
           </p>
         </div>
         <div className="image-container">
