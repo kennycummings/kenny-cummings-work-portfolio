259593a6de13c587c58d0ed31d4a45c95b32fa48
kennycummings <kennethccummings@gmail.com>
2024-06-04 15:41:12 -0400

    Added Awoken YouTube video

--- src/components/InDevelopment.jsx
+++ src/components/InDevelopment.jsx
@@ -20,6 +20,11 @@ const InDevelopment = () => {
 
         <br />
         <br />
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/lInGzFuYrj0?si=14yj5Lcv6w8kThuI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <br />
+        <br />
       </p>
     </div>
   );
